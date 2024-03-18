6da31f8cb6c5b78bb4b3bf198c9a6cd516292a82
Luann Pereira Mendes <luannpmendes@gmail.com>
2024-03-18 11:41:42 -0300

    Update import matplotlib.py

--- import matplotlib.py
+++ import matplotlib.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# Existing data
 data = {
     'Language': ['Python', 'C', 'Rust', 'JavaScript'],
     'Performance': [8, 9, 9, 6],
@@ -10,19 +11,42 @@ data = {
     'Speed': [6, 9, 9, 7]  
 }
 
-df = pd.DataFrame(data)
+# New 'Golang' row
+golang_data = {
+    'Language': ['Golang'],
+    'Performance': [9],
+    'Security': [8],
+    'Ease of Use': [8],
+    'Community': [7],
+    'Speed': [9]  
+}
 
-df['Total Score'] = df.drop('Language', axis=1).sum(axis=1)
+# Concatenate data to DataFrame
+data = pd.concat([pd.DataFrame(data), pd.DataFrame(golang_data)], ignore_index=True)
 
-df = df.sort_values(by='Total Score', ascending=False)
+# Calculate Total Score
+data['Total Score'] = data.drop('Language', axis=1).sum(axis=1)
 
+# Sort DataFrame by Total Score
+data = data.sort_values(by='Total Score', ascending=False)
+
+# Assign colors to each language
+colors = {
+    'Python': 'blue',
+    'C': 'green',
+    'Rust': 'orange',
+    'JavaScript': 'red',
+    'Golang': 'yellow'
+}
+
+# Plot
 plt.figure(figsize=(10, 6))
-bar_plot = plt.bar(df['Language'], df['Total Score'], color=['blue', 'green', 'orange', 'red'])
+bar_plot = plt.bar(data['Language'], data['Total Score'], color=[colors[lang] for lang in data['Language']])
 
 for i, bar in enumerate(bar_plot):
     yval = bar.get_height()
     plt.text(bar.get_x() + bar.get_width()/2, yval, round(yval, 2), ha='center', va='bottom')
-    plt.text(bar.get_x() + bar.get_width()/2, -3.5, f"Performance: {df['Performance'].iloc[i]}\nSecurity: {df['Security'].iloc[i]}\nEase of Use: {df['Ease of Use'].iloc[i]}\nCommunity: {df['Community'].iloc[i]}\nSpeed: {df['Speed'].iloc[i]}", ha='center', va='top')
+    plt.text(bar.get_x() + bar.get_width()/2, -3.5, f"Performance: {data['Performance'].iloc[i]}\nSecurity: {data['Security'].iloc[i]}\nEase of Use: {data['Ease of Use'].iloc[i]}\nCommunity: {data['Community'].iloc[i]}\nSpeed: {data['Speed'].iloc[i]}", ha='center', va='top')
 
 plt.xlabel('')
 plt.ylabel('Total Score')
